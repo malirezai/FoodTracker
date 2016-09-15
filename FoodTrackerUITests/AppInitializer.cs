be4edf96ceb8c4514a1adce8222efb77f075358e
malirezaie <m.alirezaie@gmail.com>
2016-09-15 13:33:31 -0700

    first commit
    test CI w/ Xcode project

---
+++ FoodTrackerUITests/AppInitializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace FoodTrackerUITests
+{
+	public class AppInitializer
+	{
+		public static IApp StartApp(Platform platform)
+		{
+
+			const string deviceIDiPhone6s = "7401F2E2-4F5C-4487-8158-9F7380492357";
+
+			// TODO: If the iOS or Android app being tested is included in the solution 
+			// then open the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			//
+			// The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+			// installed. To start the Test Cloud Agent the following code should be
+			// added to the FinishedLaunching method of the AppDelegate:
+			//
+			//    #if ENABLE_TEST_CLOUD
+			//    Xamarin.Calabash.Start();
+			//    #endif
+			if (platform == Platform.Android)
+			{
+				return ConfigureApp
+					.Android
+					// TODO: Update this path to point to your Android app and uncomment the
+					// code if the app is not included in the solution.
+					//.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+					.StartApp();
+			}
+
+			return ConfigureApp
+				.iOS
+				// TODO: Update this path to point to your iOS app and uncomment the
+				// code if the app is not included in the solution.
+				.AppBundle ("../../../xtr-FoodTracker.app")
+				.DeviceIdentifier(deviceIDiPhone6s)
+				.StartApp();
+		}
+	}
+}
+
---
+++ FoodTrackerUITests/Tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace FoodTrackerUITests
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
+
+		const int RATING = 3;
+		const string NAME = "Mixed Salad";
+
+		Query FoodCell;
+
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+			InitializeQueries();
+		}
+
+		void InitializeQueries()
+		{
+			FoodCell = x => x.Marked(NAME);
+
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
+
+		[Test]
+		public void EditFoodTest()
+		{
+			
+			app.Tap(x => x.Marked("Caprese Salad"));
+			app.Screenshot("Tapped on Caprese Salad");
+			app.Tap(x => x.Class("UITextFieldLabel").Marked("Caprese Salad"));
+			app.Screenshot("Editing Text");
+			app.ClearText(x => x.Class("UITextField").Text("Caprese Salad"));
+			app.EnterText(x => x.Class("UITextField"), "Caprese Mixed Salad");
+			app.PressEnter();
+			app.Tap(x => x.Id("filledStar").Index(RATING - 1));
+			app.Screenshot("Tapped on view with class: UIImageView");
+			app.Tap(x => x.Text("Save"));
+			app.Screenshot("Changed Name and Rating then Press Save");
+
+			//get rating showed
+			var rating = app.Query(x => x.Class("UITableViewCellContentView")
+			                       .Descendant()
+			                       .Marked(NAME)
+			                       .Sibling()
+			                       .Class("FoodTracker.RatingControl")
+			                       .Descendant()
+			                       .Id("filledStar"))
+			                	   .Count();
+
+			Assert.IsTrue(rating == RATING);
+
+		}
+
+
+	}
+}
+
